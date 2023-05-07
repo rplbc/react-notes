664bb3c46c2572a9cb00d562f90d392c571fb222
rplbc <radoslawlb@gmail.com>
2023-05-07 20:53:32 +0200

    feat: add Mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import router from '@/router'
+import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>
 )
