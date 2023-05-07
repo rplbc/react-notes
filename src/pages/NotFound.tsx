faa97798a1fe1ce7907921112cba8bead81b225f
rplbc <radoslawlb@gmail.com>
2023-05-07 23:16:46 +0200

    feat: add 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,23 @@
 import Page from '@/components/Page'
+import { Button, Flex, Title } from '@mantine/core'
+import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
   return (
     <Page title="Page not found">
-      <h1>Page not found</h1>
+      <Flex
+        align="center"
+        justify="center"
+        direction="column"
+        mih="90dvh"
+        gap="lg"
+        py="xl"
+      >
+        <Title>Page not found</Title>
+        <Button component={Link} to="/" replace>
+          ← Go back to home page
+        </Button>
+      </Flex>
     </Page>
   )
 }
